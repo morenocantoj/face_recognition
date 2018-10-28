0bd18ee28990023b5f3eb736af76eab298ab07cb
Jaime Moreno Cantó <jmc103@alu.ua.es>
2018-10-28 21:35:20 +0100

    Arguments declarated and passed in command line execution

---
+++ face_recognition_caffe.py
@@ -0,0 +1,22 @@
+import cv2
+import sys
+import ntpath
+import os
+import numpy as np
+import argparse
+
+# Get user supplied values
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+fileName = ntpath.basename(args["image"])
+
+print("Complete filename: " + fileName)
